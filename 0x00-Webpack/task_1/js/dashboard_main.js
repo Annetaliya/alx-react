b778df7dc204c1cec46b76989486b3d3c3d9f622
Annetaliya <annetaliya@gmail.com>
2023-07-24 20:10:11 +0300

    adding webpack config

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,33 +1,20 @@
-import $  from 'jquery';
-import debounce from 'lodash/debounce';
-
-
-$(function addDashboardElements() {
-  const p1 = $('<p>').text('Holberton Dashboard');
-  const p2 = $('<p>').text('Dashboard data for the students');
-  const btn = $('<button>').text('Click here to get started');
-  const p3 = $('<p>').attr('id', 'count');
-  const p4 = $('<p>').text('Copyright - Holberton School');
-
-  $('body').append(p1, p2, btn, p3, p4);
-
-  //bind debounce function to the button's click event
-  const debounceCounter = debounce(updateCounter, 500);
-
-  $('button').on('click', function () {
-    debounceCounter();
-  });
-
-});
-
+const $ = require('jquery');
+const _ = require('lodash');
 
 let count = 0;
 
-function updateCounter() {
-    count++;
-    const countParagraph = $('#count');
-    countParagraph.text(`${count} clicks on the button`);
+$('<p>Holberton Dashboard</p>').appendTo('body');
+$('<p>Dashboard data for the students</p>').appendTo('body');
+$('<button>Click here to get started</button>').appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$('<p>Copyright - Holberton School</p>').appendTo('body');
 
+function updateCounter() {
+  count++;
+  $('#count').html(`${counter} clicks on the button`);
 }
 
-$(document).ready(addDashboardElements);
+$('button').on('click', _.debounce(updateCounter, 500));
+
+
+
