2dfde42096261d52c87634a6f1074e5148ea5a4e
Annetaliya <annetaliya@gmail.com>
2023-07-30 13:52:34 +0300

    babel configauration

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: path.resolve(__dirname, '/src/index.js'),
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
         contentBase: './deploy',
@@ -27,6 +27,14 @@ module.exports = {
         { 
           test: /\.css$/, 
           use: ["style-loader", "css-loader"] 
+        
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
         {
             test: /\.(gif|png|jpe?g|svg)$/i,
