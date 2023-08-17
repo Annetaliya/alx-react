41988fc710a85d5a9f5495bc64a3baf5a8dc4d32
Annetaliya <annetaliya@gmail.com>
2023-08-17 10:48:19 +0300

    react immutable

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,21 +1,5 @@
 import { Map } from 'immutable';
 
-function mergeDeeplyRecursive(target, source) {
-    return target.withMutations(map => {
-        for (const key of source.keys()) {
-            if (map.has(key) && Map.isMap(map.get(key)) && Map.isMap(source.get(key))) {
-                map.set(key, mergeDeeplyRecursive(map.get(key), source.get(key)));
-            } else {
-                map.set(key, source.get(key));
-            }
-        }
-    });
-}
-
-export function mergeDeeplyElements(page1, page2) {
-    const map1 = Map(page1);
-    const map2 = Map(page2);
-    
-    const mergedMap = mergeDeeplyRecursive(map1, map2);
-    return List(mergedMap.values());
+export default function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(Map(page2));
 }
